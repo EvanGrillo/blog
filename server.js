f2366606e719ef9b3d07cac728936e32a6053f9b
EvanGrillo <evan.grillo@outlook.com>
2021-10-28 17:20:03 -0500

    Start to design & implement simple blog

---
+++ server.js
@@ -0,0 +1,126 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const server = http.createServer(() => {});
+const port = process.env.PORT || 3000;
+const mimeTypes = require('./mimeTypes');
+let db = require('./db.js');
+
+connectToDB = async () => {
+    try {
+        await db.connect();
+    } catch (err) {
+        console.log("\x1b[31m", err);
+        throw err;
+    }
+}
+connectToDB().then(() => {
+    server.listen(port);
+});
+
+server.on('request', (req, res) => {
+
+    let { url } = req;
+
+    if ((/public/).test(url)) url = '.' + url;
+
+    if (url === '/') {
+        url = './public/index.html';
+        return renderBlogs(res, url);
+    }
+
+    if ((/blog/.test(url)) && !(/style/).test(url)) {
+        return renderBlog(res, `./public${url}`);
+    }``
+
+    let extname = path.extname(url);
+    let contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    try {
+
+        let content = fs.readFileSync(url);
+        res.writeHead(200, { 'Content-Type': contentType });
+        return res.end(content, 'utf8');
+
+    } catch (err) {
+        return sendError(err);
+    }
+
+});
+
+renderBlogs = async (res, url) => {
+
+    console.log("\x1b[32m", url, '>> RenderBlogs');
+
+    try {
+
+        let blogs = await db.getBlogs();
+
+        let blogPreviews = '';
+        let blogPreviewTemplate = fs.readFileSync('./public/templates/blogPreview.html', 'utf8');
+
+        for (blog of blogs) {
+
+            let entry = blogPreviewTemplate
+            .replace('[title]', blog.title)
+            .replaceAll('[titleLink]', blog.title.toLowerCase().split(' ').join('-'))
+            .replace('[createdDate]', blog.createdDate);
+
+            blogPreviews = blogPreviews.concat(entry);
+        }
+
+        let content = 
+        fs.readFileSync(url, 'utf8')
+        .replace('[blogs]', blogPreviews);
+
+        return res.end(content, 'utf8');
+
+    } catch (err) {
+        console.log("\x1b[31m", err);
+        sendError(res, err);
+    }
+
+}
+
+renderBlog = async (res, url) => {
+
+    let handle = url.split('/')[3];
+
+    console.log("\x1b[32m", url, handle, '>> RenderBlog');
+
+    try {
+
+        let blog = await db.getBlog_ByHandle(handle);
+
+        let blogDisplayTemplate = fs.readFileSync('./public/templates/blogDisplay.html', 'utf8');
+
+        let blogDisplay = blogDisplayTemplate
+        .replace('[title]', blog.title)
+        .replace('[text]', blog.text)
+        .replace('[createdDate]', blog.createdDate);
+
+        let content = 
+        fs.readFileSync('./public/index.html', 'utf8')
+        .replace('[blogs]', blogDisplay);
+
+        return res.end(content, 'utf8');
+
+    } catch (err) {
+        console.log("\x1b[31m", err);
+        sendError(res, err);
+    }
+
+}
+
+sendError = async (res, err) => {
+
+    console.log("\x1b[31m", err, '>> sendError');
+
+    if (err.code == 'ENOENT' || 'EISDIR') {
+        return res.end('<h1>404 Forbidden</h1>', 'utf-8');
+    }
+    return res.end('<h1>500</h1>', 'utf-8');
+
+}
+
+console.log(`Server listening at port:${port}/`);
