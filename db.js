ab1f7937721c084da6d646ce0dc04983536227b2
EvanGrillo <evan.grillo@outlook.com>
2021-11-01 17:24:05 -0500

    Add collection & query method

--- db.js
+++ db.js
@@ -3,30 +3,40 @@ const uri = "mongodb://localhost:27017/";
 
 const mongoConnect = {
 	blogs: null,
-    connect: async () => {
-	    try {
+	users: null,
+	connect: async () => {
+		try {
 			let dbo = await MongoClient.connect(uri);
-            this.blogs = dbo.db('blog').collection('blogs');
-	    } catch (err) {
+			this.blogs = dbo.db('blog').collection('blogs');
+			this.users = dbo.db('blog').collection('users');
+		} catch (err) {
 			throw err;
 		}
 	},
-    getBlogs: async () => {
-        return await new Promise((resolve, reject) => {
-            this.blogs.find({}).toArray((err, result) => {
-                if (err) reject(err);
-                resolve(result);
-            });
-        });
-    },
-    getBlog_ByHandle: async (handle) => {
-        return await new Promise((resolve, reject) => {
-            this.blogs.findOne({handle: handle}, (err, result) => {
-                if (err) reject(err);
-                resolve(result);
-            });
-        });
-    }
+	getBlogs: async () => {
+		return await new Promise((resolve, reject) => {
+			this.blogs.find({}).toArray((err, result) => {
+				if (err) reject(err);
+				resolve(result);
+			});
+		});
+	},
+	getBlog_ByHandle: async (handle) => {
+		return await new Promise((resolve, reject) => {
+			this.blogs.findOne({handle: handle}, (err, result) => {
+				if (err) reject(err);
+				resolve(result);
+			});
+		});
+	},
+	getUser_ByEmail: async (email) => {
+		return await new Promise((resolve, reject) => {
+			this.users.findOne({email: email}, (err, result) => {
+				if (err) reject(err);
+				resolve(result);
+			});
+		});
+	}
 };
 
 module.exports = mongoConnect;
