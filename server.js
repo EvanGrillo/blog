99e08f9bfc0a183fc703e9987565191269216420
EvanGrillo <evan.grillo@outlook.com>
2021-11-08 16:58:11 -0600

    Rework server file

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const http = require('http');
 const server = http.createServer(() => {});
 const port = process.env.PORT || 3000;
 const db = require('./middleware/db.js');
-const mailer = require('./middleware/email.js');
-const auth = require('./middleware/auth.js');
-const renderer = require('./middleware/renderer.js');
-const readFile = require('./utils/readFile.js');
+const GET = require('./middleware/CRUD/GET.js');
+const POST = require('./middleware/CRUD/POST.js');
 
 connectToDB = async () => {
     try {
@@ -23,35 +21,11 @@ connectToDB().then(() => {
 
 server.on('request', (req, res) => {
 
-    let { method, url } = req;
+    const { method } = req;
 
-    if ((/public/).test(url)) url = '.' + url;
+    if (method === 'GET') return GET(req, res);
 
-    if (url === '/'|| url === '/blog') {
-        return renderer.homeView(req, res);
-    }
-
-    if ((/blog/.test(url)) && !(/style/).test(url)) {
-        return renderer.displayAsset(res, `./public${url}`);
-    }
-
-    if ((/admin/).test(url) && method === 'GET') {
-        return auth.checkForToken(req, res);
-    }
-
-    if ((/authGEO/).test(url) && method === 'POST') {
-        return auth.validateGEO(req, res);
-    }
-
-    if ((/login/).test(url) && method === 'POST') {
-        return auth.passwordMatch(req, res);
-    }
-
-    if ((/send/).test(url) && method === 'POST') {
-        return mailer.sendMessage(req, res);
-    }
-
-    readFile(res, url);
+    if (method === 'POST') return POST(req, res);
 
 });
 
